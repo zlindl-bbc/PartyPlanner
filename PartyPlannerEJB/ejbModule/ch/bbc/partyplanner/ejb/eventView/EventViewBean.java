9ba25b03b541814b94a49fa7609db491cffd8d68
zlindl-bbc <luca.lindegger@bbcag.ch>
2016-12-05 13:09:42 +0100

    Added EventViewBean & EventViewBeanLocal

---
+++ PartyPlannerEJB/ejbModule/ch/bbc/partyplanner/ejb/eventView/EventViewBean.java
@@ -0,0 +1,23 @@
+package ch.bbc.partyplanner.ejb.eventView;
+
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Session Bean implementation class RegisterBean
+ */
+@Stateless
+public class EventViewBean implements EventViewBeanLocal {
+
+	private final static Logger LOGGER = Logger.getLogger(EventViewBean.class.getName());
+	
+	@PersistenceContext
+	EntityManager em;
+
+	public EventViewBean() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ PartyPlannerEJB/ejbModule/ch/bbc/partyplanner/ejb/eventView/EventViewBeanLocal.java
@@ -0,0 +1,12 @@
+package ch.bbc.partyplanner.ejb.eventView;
+
+import javax.ejb.Local;
+
+@Local
+public interface EventViewBeanLocal {
+
+	/**
+	 * 
+	 * @param event
+	 */
+}
