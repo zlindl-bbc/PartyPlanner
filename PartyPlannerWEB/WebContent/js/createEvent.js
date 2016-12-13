920b9c5bfd17f5413b155671411c729cf109aec6
zlindl-bbc <luca.lindegger@bbcag.ch>
2016-12-13 13:44:38 +0100

    Create new event

---
+++ PartyPlannerWEB/WebContent/js/createEvent.js
@@ -0,0 +1,15 @@
+function newProductRow() {
+	var th = document.querySelectorAll('#table th');
+
+	if (!th.length) {
+		var rows = "<th>Produktname</th><th>Anzahl</th>";
+		var table = document.createElement('table');
+		table.innerHTML = rows;
+		document.getElementById("table").appendChild(table.firstChild);
+	}
+	
+	var newRow = "<tr><th><input type='text' placeholder='Produktname'></th><th><input type='number' value='0'></th></tr>";
+	var table = document.createElement('table');
+	table.innerHTML = newRow;
+	document.getElementById("table").appendChild(table.firstChild);
+}
